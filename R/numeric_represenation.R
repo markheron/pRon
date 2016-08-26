eaf892be20dbfea4ca164b41ac2da21159dd712c
Mark Heron <heron@genzentrum.lmu.de>
2016-08-26 13:40:04 +0200

    add new fasta2num method which reliese on the internal representation of DNAString add higher order logo (still needs some merging)

--- R/numeric_represenation.R
+++ R/numeric_represenation.R
@@ -6,18 +6,25 @@
 #' Any oligo-nucleotide with a letter differing from A,C,G,T is represented as **NA**
 #' 
 #' @export
-#' @param fastas (DNAStringSet) of same length fasta sequences that should be converted
+#' @param fastas (DNAStringSet) of same length fasta sequences that should be converted (single DNAString also possible)
 #' @param oligo_length (numeric) length of the oligo-nucleotides to use.
-#' @param method ("lookupTable") one of "lookupTable", "slidingView" and "memoryLimited" they are different implementations that have different speed/memory tradeoffs
+#' @param method ("as.integer") one of"as.integer", "lookupTable", "slidingView" and "memoryLimited" they are different implementations that have different speed/memory tradeoffs
 #' @param simplify (bool) in the case of a single fasta sequence, should a vector be returned instead of an matrix
 #' @return matrix of numeric representation of the fastas
 #' 
-fasta2num <- function (fastas, oligo_length, method="lookupTable", simplify=TRUE) {
+fasta2num <- function (fastas, oligo_length, method="as.integer", simplify=TRUE) {
+  
+  if(class(fastas) == "DNAString") {
+    fastas <- DNAStringSet(fastas)
+  }
   
   # could use chartr instead of this, but I have to strsplit them anyway, so this probably has similar speed
   base2num <- c("A"=1, "C"=2, "G"=3, "T"=4, "N"=NA)
   
-  if(method == "slidingView") {
+  xinteger2num <- c(1,2,NA,3,NA,NA,NA,4)
+  if(method == "as.integer") { # kind of a hack, numbering could change in the future
+    seqnum_zero <- sapply(fastas, function(x) xinteger2num[as.integer(x)])
+  } else if(method == "slidingView") {
     seqnum_zero <- sapply(fastas, function (x) colSums(t(letterFrequencyInSlidingView(x, 1, c("A","C","G","T")))*(1:4)))
   } else if (method == "lookupTable") { # previous line implementation is faster, see benchmark  (on human sized fasta's this seems faster and has less memory footprint)
     seqnum_zero <- sapply(fastas, function (fasta) base2num[strsplit(as.character(fasta),"")[[1]]])
@@ -32,6 +39,7 @@ fasta2num <- function (fastas, oligo_length, method="lookupTable", simplify=TRUE
       return( tmp )
     })
   }
+  rownames(seqnum_zero) <- NULL # because they don't make sense
 
   seqnum_zero[seqnum_zero == 0] <- NA
   
