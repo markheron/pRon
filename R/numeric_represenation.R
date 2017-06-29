e7a4fef406a335b7f7668557cc12d4c351da4da3
Mark Heron <heron@genzentrum.lmu.de>
2017-06-29 17:05:57 +0200

    Clean and improve numeric_representation.R
    Fix fasta2num 'lookupTable' option which was broken.
    Remove cut_out_sequence_from_one_chr because likely unused since it is broken.
    Add evenfasta2num function.

--- R/numeric_represenation.R
+++ R/numeric_represenation.R
@@ -8,14 +8,14 @@
 #' @export
 #' @param fastas (DNAStringSet) of same length fasta sequences that should be converted (single DNAString also possible)
 #' @param oligo_length (numeric) length of the oligo-nucleotides to use.
-#' @param method ("as.integer") one of"as.integer", "lookupTable", "slidingView" and "memoryLimited" they are different implementations that have different speed/memory tradeoffs
-#' @param simplify (bool) in the case of a single fasta sequence, should a vector be returned instead of an matrix
-#' @return matrix of numeric representation of the fastas
+#' @param method ("lookupTable") one of"as.integer", "lookupTable", "slidingView" and "memoryLimited" they are different implementations that have different speed/memory tradeoffs
+#' @param simplify (bool) in the case of a single fasta sequence, should a vector be returned instead of a matrix
+#' @return matrix (or vector) of numeric representation of the fastas
 #' 
-fasta2num <- function (fastas, oligo_length, method="as.integer", simplify=TRUE) {
+fasta2num <- function (fastas, oligo_length, method="lookupTable", simplify=TRUE) {
   
   if(class(fastas) == "DNAString") {
-    fastas <- DNAStringSet(fastas)
+    fastas <- Biostrings::DNAStringSet(fastas)
   }
   
   # could use chartr instead of this, but I have to strsplit them anyway, so this probably has similar speed
@@ -24,10 +24,14 @@ fasta2num <- function (fastas, oligo_length, method="as.integer", simplify=TRUE)
   xinteger2num <- c(1,2,NA,3,NA,NA,NA,4)
   if(method == "as.integer") { # kind of a hack, numbering could change in the future
     seqnum_zero <- sapply(fastas, function(x) xinteger2num[as.integer(x)])
+    
   } else if(method == "slidingView") {
     seqnum_zero <- sapply(fastas, function (x) colSums(t(letterFrequencyInSlidingView(x, 1, c("A","C","G","T")))*(1:4)))
+    
   } else if (method == "lookupTable") { # previous line implementation is faster, see benchmark  (on human sized fasta's this seems faster and has less memory footprint)
     seqnum_zero <- sapply(fastas, function (fasta) base2num[strsplit(as.character(fasta),"")[[1]]])
+    # seqnum_zero <- sapply(strsplit(as.character(fastas),""), function (fasta) base2num[fasta]) # similar speed
+    
   } else if (method == "memoryLimited") {
       
     seqnum_zero <- sapply(fastas, function (fasta) {
@@ -56,14 +60,40 @@ fasta2num <- function (fastas, oligo_length, method="as.integer", simplify=TRUE)
   return(seqnum_higher)
 }
 
+  
+
+
+
+#' evenfasta2num
+#' 
+#' Transforms a DNAStringSet into a list of numeric representation of the mono-nucleotides.
+#' Any mono-nucleotide that isn't A,C,G or T is represented as **NA**.
+#' This is a streamlined version of \code{\link{fasta2num}} for mono-nucleotides that additionally allows varying sequence lengths.
+#'
+#' @export
+#' @param fastas (DNAStringSet) that can be different length DNA sequences that should be converted (single DNAString also possible)
+#' @return list of numeric representation of the DNA sequences
+#'
+evenfasta2num <- function (fastas) {
+  
+  # could use chartr instead of this, but I have to strsplit them anyway, so this probably has similar speed
+  base2num <- c("A"=1, "C"=2, "G"=3, "T"=4, "N"=NA)
+  
+  seqnum_zero <- lapply(strsplit(as.character(fastas),""), function (x) base2num[x])
+  
+  return(seqnum_zero)
+}
+
+
 
 
 
 #' num2freq
 #'
-#' Calculates the mere frequencies given a oligo-nucleotide-coded representation matrix
+#' Calculates the oligo-nucleotide frequencies given a numerically encoded matrix representation.
+#' 
 #' @export
-#' @param seqnum (numeric matrix) oligo-nucleotide-coded representation of fasta sequences
+#' @param seqnum (numeric matrix) numerically encoded oligo-nucleotide representation of DNA sequences
 #' @param oligo_length (integer) length of the oligonucleotide-coding used (order+1)
 #' @return matrix of oligonucleotide frequencies
 #' 
@@ -78,11 +108,11 @@ num2freq <- function(seqnum, oligo_length) {
 
 #' num2weightedfreq
 #'
-#' Calculates the weighted mere frequencies given a oligonucleotide-coded representation matrix
+#' Calculates the weighted oligo-nucleotide frequencies given a numerically encoded matrix representation.
+#' 
 #' @export
-#' @param seqnum (numeric matrix) oligo-nucleotide-coded representation of fasta sequences
-#' @param weights of the different fasta sequences
-#' @param oligo_length (integer) length of the oligonucleotide-coding used (order+1)
+#' @inheritParams num2freq
+#' @param weights (numeric) to be given to the fasta sequences (should have length matching \code{ncol(seqnum)})
 #' @return matrix of weighted oligonucleotide frequencies
 #' 
 num2weightedfreq <- function(seqnum, weights, oligo_length) {
@@ -91,28 +121,4 @@ num2weightedfreq <- function(seqnum, weights, oligo_length) {
   freqs <- sapply(1:length(oligos), function (oligo) colSums(apply((seqnum==oligo), 1, function (x) x*weights), na.rm=TRUE))
   colnames(freqs) <- oligos
   return(t(freqs))
-}
-
-
-
-
-#' cut_out_seqnums_from_one_chr
-#'
-#' Cut's out multiple regions from a single oligonucleotide-coded respresentation vector (i.e. typically chromosome).
-#' export not for now
-#' @param pos vector of central position for cuting out the region
-#' @param strand vector of "+"/"-" to specify if the forward or the reverse complement sequence shall be used
-#' @param size region size before and after the positions to cut out
-#' @param order what oligonucleotide order was used to code the fasta sequence
-#' @param chr_num oligonucleotide-coded respresentation of the chromosome from which to cut out the regions
-#' @return matrix of cut out fragment
-#' 
-cut_out_seqnums_from_one_chr <- function(pos, strand, size, order, chr_num) {
-  
-  # does this really do what it should do, if yes, why seqs should be over written and it isn't devined to start out with...?
-  for(i in 1:length(seqs)) {
-    
-    seqs <-  vapply(pos, function (i) chr_num[i+(-size -((strand=="-")*order)):(size +((strand=="+")*order))], FUN.VALUE=rep(0, length((-size -((strand=="-")*order)):(size +((strand=="+")*order)))))
-  }
-  return(seqs)
 }
